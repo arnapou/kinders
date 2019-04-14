be449319099d9123bddc8fe9ff8f6e75af39a91d
Arnaud Buathier <arnaud@arnapou.net>
2019-04-14 16:19:22 +0200

    init

---
+++ Install.md
@@ -0,0 +1,16 @@
+
+Dependencies
+
+    sudo aptitude install nodejs
+    
+    curl -o- -L https://yarnpkg.com/install.sh | bash
+    
+    sudo aptitude install nodejs npm
+    npm install npm@latest -g
+
+Create admin react project
+
+    npx create-react-app admin
+    cd admin
+    yarn add @api-platform/admin
+    
---
+++ admin/build/precache-manifest.b42c39df74ca9e8be6c390092452c0b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1bea54dfd945d9e89856",
+    "url": "/static/js/main.f2f65807.chunk.js"
+  },
+  {
+    "revision": "fabda92e5c74184d65bb",
+    "url": "/static/js/2.af86c1e2.chunk.js"
+  },
+  {
+    "revision": "1bea54dfd945d9e89856",
+    "url": "/static/css/main.44556985.chunk.css"
+  },
+  {
+    "revision": "ffcf30f54125e8c9b0f9470af9b8ef17",
+    "url": "/index.html"
+  }
+];
