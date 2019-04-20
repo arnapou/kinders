9c874529ee55716319813d32cd3649ca058730d5
Arnaud Buathier <arnaud@arnapou.net>
2019-04-20 16:31:22 +0200

    reinit project

--- admin/build/env.js
+++
@@ -1,8 +0,0 @@
-
-var API_HOSTNAME = 'kinders.arnapou.local';
-
-window.env = {
-    // This option can be retrieved in "src/index.js" with "window.env.API_URL".
-    API_BASE_URL:    'http://' + API_HOSTNAME + '/api',
-    API_LOGIN_CHECK: 'http://' + API_HOSTNAME + '/login_check'
-};
