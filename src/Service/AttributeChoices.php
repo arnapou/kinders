16d48879757a3fe26af0b986c30419c06d8fb13a
Arnaud Buathier <arnaud@arnapou.net>
2019-05-02 01:10:17 +0200

    attribute chunk amelioration

---
+++ src/Service/AttributeChoices.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the Arnapou Kinders package.
+ *
+ * (c) Arnaud Buathier <arnaud@arnapou.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Service;
+
+use Symfony\Component\Form\FormView;
+
+class AttributeChoices
+{
+    const MAX_COLUMNS = 10;
+    const MIN_COLUMN_COUNT = 5;
+    /**
+     * @var array
+     */
+    private $numericTypes = ['poids'];
+
+    public function getChunks(FormView $formView)
+    {
+        $choiceTypes = $this->getSortedChoicesTypes($formView);
+
+        $items = [];
+        foreach ($choiceTypes as $type => $choices) {
+            $items[] = ['title' => $type];
+            foreach ($choices as $id => $choice) {
+                $items[] = ['choice' => $this->findChild($formView, $id)];
+            }
+        }
+        return $this->chunk($items);
+    }
+
+    private function getSortedChoicesTypes(FormView $formView)
+    {
+        $choiceTypes = [];
+        foreach ($formView->vars['choices'] as $choices) {
+            foreach ($choices as $choice) {
+                $choiceTypes[$choices->label][$choice->value] = $choice->label;
+            }
+            if (\in_array($choices->label, $this->numericTypes)) {
+                asort($choiceTypes[$choices->label], SORT_NUMERIC);
+            } else {
+                asort($choiceTypes[$choices->label], SORT_STRING | SORT_FLAG_CASE);
+            }
+        }
+        ksort($choiceTypes, SORT_STRING | SORT_FLAG_CASE);
+        return $choiceTypes;
+    }
+
+    private function findChild(FormView $formView, int $id)
+    {
+        foreach ($formView as $child) {
+            if ($child->vars['value'] == $id) {
+                return $child;
+            }
+        }
+        return null;
+    }
+
+    private function chunk(array $items): array
+    {
+        if (\count($items) <= self::MIN_COLUMN_COUNT) {
+            return [$items];
+        }
+        $nbColumns = ceil(\count($items) / self::MIN_COLUMN_COUNT);
+        $nbColumns = $nbColumns <= self::MAX_COLUMNS ? $nbColumns : self::MAX_COLUMNS;
+        $chunkSize = ceil(\count($items) / $nbColumns);
+        $chunks    = [];
+        $chunk     = [];
+        $chunked   = 0;
+        foreach ($items as $item) {
+            if (\count($chunks) < $nbColumns &&
+                (
+                    // max size chunk atteint
+                    \count($chunk) == $chunkSize ||
+                    // un title est le dernier element du chunk => on le passe a la colonne suivante
+                    \count($chunk) == $chunkSize - 1 && isset($item['title']) ||
+                    // un title dans l'avant derniere colonne avec suffisamment de place pour passer le reste dans la derniere colonne
+                    \count($chunks) == $nbColumns - 2 && isset($item['title']) && \count($items) - $chunked <= $chunkSize
+                )
+            ) {
+                $chunks[] = $chunk;
+                $chunk    = [];
+            }
+            $chunk[] = $item;
+            $chunked++;
+        }
+        if ($chunk) {
+            $chunks[] = $chunk;
+        }
+
+        return $chunks;
+    }
+}
--- src/Service/TwigExtension.php
+++ src/Service/TwigExtension.php
@@ -11,15 +11,21 @@
 
 namespace App\Service;
 
-use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 use Symfony\Component\Form\FormView;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class TwigExtension extends AbstractExtension
 {
-    const ATTRIBUTES_MAX_COLUMNS = 10;
-    const ATTRIBUTES_MIN_COLUMN_COUNT = 5;
+    /**
+     * @var AttributeChoices
+     */
+    private $attributeChoices;
+
+    public function __construct(AttributeChoices $attributeChoices)
+    {
+        $this->attributeChoices = $attributeChoices;
+    }
 
     public function getFilters()
     {
@@ -30,52 +36,9 @@ class TwigExtension extends AbstractExtension
         ];
     }
 
-    public function attributechunks(FormView $view)
+    public function attributechunks(FormView $formView)
     {
-        $choiceTypes = $view->vars['choices'];
-        ksort($choiceTypes);
-
-        $findChild = function (ChoiceView $choice) use ($view) {
-            foreach ($view as $child) {
-                if ($child->vars['value'] == $choice->value) {
-                    return $child;
-                }
-            }
-            return null;
-        };
-
-        $items = [];
-        foreach ($choiceTypes as $type => $choices) {
-            $items[] = ['title' => $type];
-            foreach ($choices as $choice) {
-                $items[] = ['choice' => $findChild($choice)];
-            }
-        }
-
-        if (\count($items) <= self::ATTRIBUTES_MIN_COLUMN_COUNT) {
-            return [$items];
-        }
-
-        $nbColumns = ceil(\count($items) / self::ATTRIBUTES_MIN_COLUMN_COUNT);
-        $nbColumns = $nbColumns <= self::ATTRIBUTES_MAX_COLUMNS ? $nbColumns : self::ATTRIBUTES_MAX_COLUMNS;
-        $chunkSize = ceil(\count($items) / $nbColumns);
-
-        $chunks = [];
-        $chunk  = [];
-        foreach ($items as $item) {
-            if (\count($chunk) == $chunkSize ||
-                \count($chunk) == $chunkSize - 1 && isset($item['title'])
-            ) {
-                $chunks[] = $chunk;
-                $chunk    = [];
-            }
-            $chunk[] = $item;
-        }
-        if ($chunk) {
-            $chunks[] = $chunk;
-        }
-
-        return $chunks;
+        return $this->attributeChoices->getChunks($formView);
     }
 
     public function thumbnail($filename)
