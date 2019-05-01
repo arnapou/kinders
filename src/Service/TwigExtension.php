3555ce17ebdacb0a7a7d04124eaf94fb639be585
Arnaud Buathier <arnaud@arnapou.net>
2019-05-02 00:26:14 +0200

    better attributes list

--- src/Service/TwigExtension.php
+++ src/Service/TwigExtension.php
@@ -11,17 +11,71 @@
 
 namespace App\Service;
 
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
+use Symfony\Component\Form\FormView;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class TwigExtension extends AbstractExtension
 {
+    const ATTRIBUTES_MAX_COLUMNS = 10;
+    const ATTRIBUTES_MIN_COLUMN_COUNT = 5;
+
     public function getFilters()
     {
         return [
             new TwigFilter('tn', [$this, 'thumbnail']),
             new TwigFilter('imagetype', [$this, 'imagetype']),
+            new TwigFilter('attributechunks', [$this, 'attributechunks']),
         ];
+    }
+
+    public function attributechunks(FormView $view)
+    {
+        $choiceTypes = $view->vars['choices'];
+        ksort($choiceTypes);
+
+        $findChild = function (ChoiceView $choice) use ($view) {
+            foreach ($view as $child) {
+                if ($child->vars['value'] == $choice->value) {
+                    return $child;
+                }
+            }
+            return null;
+        };
+
+        $items = [];
+        foreach ($choiceTypes as $type => $choices) {
+            $items[] = ['title' => $type];
+            foreach ($choices as $choice) {
+                $items[] = ['choice' => $findChild($choice)];
+            }
+        }
+
+        if (\count($items) <= self::ATTRIBUTES_MIN_COLUMN_COUNT) {
+            return [$items];
+        }
+
+        $nbColumns = ceil(\count($items) / self::ATTRIBUTES_MIN_COLUMN_COUNT);
+        $nbColumns = $nbColumns <= self::ATTRIBUTES_MAX_COLUMNS ? $nbColumns : self::ATTRIBUTES_MAX_COLUMNS;
+        $chunkSize = ceil(\count($items) / $nbColumns);
+
+        $chunks = [];
+        $chunk  = [];
+        foreach ($items as $item) {
+            if (\count($chunk) == $chunkSize ||
+                \count($chunk) == $chunkSize - 1 && isset($item['title'])
+            ) {
+                $chunks[] = $chunk;
+                $chunk    = [];
+            }
+            $chunk[] = $item;
+        }
+        if ($chunk) {
+            $chunks[] = $chunk;
+        }
+
+        return $chunks;
     }
 
     public function thumbnail($filename)
