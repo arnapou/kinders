8418d3a0a8f22b330965969a04ebb6548243db81
Arnaud Buathier <arnaud@arnapou.net>
2019-05-25 19:42:34 +0200

    pb chown

--- mep.sh
+++ mep.sh
@@ -8,5 +8,6 @@ sudo bin/console doctrine:schema:update --force --dump-sql
 sudo /root/.yarn/bin/yarn install
 sudo /root/.yarn/bin/yarn encore production
 sudo chown www-data:www-data -R .
+sudo chown www-data:www-data -R /cache/kinders
 
 
