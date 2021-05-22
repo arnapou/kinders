5cc69abce8b3692fbaddf92ff86cf783f63a2ceb
Arnaud Buathier <arnaud@arnapou.net>
2021-05-22 21:45:07 +0200

    feat: old forum redirects

---
+++ public/announcements.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @deprecated This script is used to redirect the old forum forum.arnapou.net
+ */
+class announcements
+{
+}
+
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: /");
